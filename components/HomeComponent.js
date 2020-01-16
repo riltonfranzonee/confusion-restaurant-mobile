a4d31aa9f69e09ce50088b863c8f6ddf7fd849c6
Rilton Franzone <riltonfranzone@gmail.com>
2020-01-16 20:49:15 -0300

    update home page

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,15 +1,47 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {ScrollView, Text} from 'react-native'
+import {Card} from 'react-native-elements'
+import {DISHES} from '../shared/dishes'
+import {PROMOTIONS} from '../shared/promotions'
+import {LEADERS} from '../shared/leaders'
+
+function RenderItem({item}){
+    if(item != null){
+        return(
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation ? item.designation : null}
+                image={require('./images/uthappizza.png')}
+            >
+            <Text style={{margin: 10}}>{item.description}</Text>
+            </Card>
+        )
+
+    } else{
+        return <View></View>
+    }
+}
+
 
 export default class Home extends React.Component{
     
+    state={
+        dishes: DISHES,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+    }
+
     static navigationOptions = {
         title: 'Home'
     }
 
     render(){
         return(
-            <View><Text>Home Component</Text></View>
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter(dish => dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter(promotion => promotion.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter(leader => leader.featured)[0]} />
+            </ScrollView>
         )
     }
 
