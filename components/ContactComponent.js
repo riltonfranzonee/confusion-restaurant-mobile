716eb3849b6764f7200b7a3bf7d9bd3c3ddf3362
Rilton Franzone <riltonfranzone@gmail.com>
2020-01-21 21:31:54 -0300

    expo mailcomposer

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import {Card} from 'react-native-elements'
-import {Text, ScrollView} from 'react-native'
+import {Card, Icon} from 'react-native-elements'
+import {Text, ScrollView, TouchableOpacity, StyleSheet} from 'react-native'
 import * as Animatable from 'react-native-animatable'
-
+import * as MailComposer from 'expo-mail-composer';
 
 export default class Contact extends React.Component{
+
+    sendMail(){
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern'
+        })
+    }
 
     render(){
         return(
@@ -20,9 +28,31 @@ export default class Contact extends React.Component{
                         <Text style={{margin: 10}}>Fax: +852 8765 4321</Text>
                         <Text style={{margin: 10}}>Fax: +852 8765 4321</Text>
                         <Text style={{margin: 10}}>Email: confusion@food.net</Text>
+                        <TouchableOpacity style={styles.btn} onPress={this.sendMail}>
+                            <Icon name='envelope-o' color='white' type='font-awesome' size={20} />
+                            <Text style={styles.btnText}>Email</Text>
+                        </TouchableOpacity>
                     </Card>
                 </Animatable.View>
             </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    btn:{
+        backgroundColor: '#521da8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginTop: 10,
+        height: 40,
+        borderRadius: 5
+    },
+    btnText:{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 17,
+        paddingLeft: 5
+    }
+})
